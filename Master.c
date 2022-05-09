3cf28815a41e67088279bf137d23012066c9f3e8
PKNate <arturoraal@hotmail.es>
2022-05-08 22:30:43 -0500

    First version. Established connection between all four PICs via SPI

--- Master.c
+++ Master.c
@@ -1,85 +1,105 @@
 #include <18f4550.h>
-#fuses HSPLL, PLL2, CPUDIV1, NOPROTECT, NOWDT, NOMCLR, NOLVP
-#use delay (clock=48M)
-#use rs232(rcv=pin_c7,xmit=pin_c6,baud=9600,bits=8,parity=n)
+#fuses INTRC_IO, CPUDIV1, NOPROTECT, NOWDT, NOMCLR, NOLVP
+#use delay (clock=8M)
+#use rs232(xmit = PIN_D0, rcv = PIN_D1, baud =9600, bits = 8, parity = N, stream = TTL)
+#include <string.h>
+#include <stdlib.h>
 
-#byte PORTA = 0xF80
-#byte PORTB = 0xF81
-#byte PORTC = 0xF82
-#byte PORTD = 0xF83
-#byte PORTE = 0xF84
+#define S_SENSOR PIN_C0
+#define S_GLCD PIN_C1
+#define S_MOTOR PIN_C2
 
-#byte TRISA = 0xF92
-#byte TRISB = 0xF93
-#byte TRISC = 0xF94
-#byte TRISD = 0xF95
-#byte TRISE = 0xF96
+//RCSTA: RECEIVE STATUS AND CONTROL REGISTER
 
-#bit LEFT = 0xF80.0
-#bit LFFR = 0xF80.1
-#bit FRNT = 0xF80.2
-#bit RGFR = 0xF80.3
-#bit RGHT = 0xF80.5
+#bit CREN=0xFAB.4    // Continuous Receive Enable Bit
+#bit OERR=0xFAB.1    // Overrun Error Bit
 
-#bit PWMA = 0xF82.2
-#bit PWMB = 0xF82.1
-#bit BI1 = 0xF83.0
-#bit BI2 = 0xF83.1
-#bit AI1 = 0xF84.0
-#bit AI2 = 0xF84.1
+/*
+// comandos
+#define ADC    1
+#define CONT   2
+#define PUSH   3
+*/
 
-#bit STBY = 0xF84.2
-
-void setup();
-void motorDirection(char M1, char M2, int pwm1, int pwm2);
+void fflush();
 
 void main()
 {
-   setup();
-   printf("Inicio\n\r");
+   int i;
+   short notConnected=1;
+   char c, s[50];
+   
+   output_high(S_SENSOR);
+   output_high(S_GLCD);
+   output_high(S_MOTOR);
+   output_high(PIN_D6);
+   setup_spi(spi_master | spi_l_to_h | spi_clk_div_4);
+   
+   while(notConnected)
+   {
+      fflush();
+      if(kbhit())
+      {
+         gets(s);                        
+         if(s[1] == '+' && s[2] == 'C' && s[3] == 'O' && s[4] == 'N' && s[5] == 'N'&& s[6] == 'E' && s[7] == 'C' && s[8] == 'T' && s[9] == 'E' && s[10] == 'D')
+            notConnected=0;
+         else
+         for(i=0;i<strlen(s);i++)
+            s[i]='\0';
+      }
+   }
+   
+   printf("Connected succesfully\n\r");
+   
    while(true)
    {
-     printf("Dentro\n\r");
-     delay_ms(500);
+      output_low(PIN_D6);
+      output_high(PIN_D7);
+      
+      fflush();
+      if(kbhit())
+      {
+         c=getc();
+         switch(c)
+         {
+            case '1':
+            {
+               printf("Command received: %c\n\r",c);
+               output_low(S_SENSOR);
+               spi_write(1);
+               output_high(S_SENSOR);
+               break;
+            }
+            
+            case '2':
+            {
+               printf("Command received: %c\n\r",c);
+               output_low(S_GLCD);
+               spi_write(2);
+               output_high(S_GLCD);
+               break;
+            }
+            
+            case '3':
+            {
+               printf("Command received: %c\n\r",c);
+               output_low(S_MOTOR);
+               spi_write(3);
+               output_high(S_MOTOR);
+               break;
+            }
+         }
+      }
    }
 }
 
-void setup()
+void fflush()
 {
-   printf("Setup\n\r");
-   
-   TRISA=0b11111111;
-   TRISB=0b11111101; //SCL
-   TRISC=0b10111001; //PWMA, PWMB, TX
-   TRISD=0b11111100; //BI1, BI2
-   TRISE=0b01111000; //STBY, AI1, AI2
-   
-   setup_timer_2(T2_DIV_BY_1,255,1);
-   setup_ccp1(CCP_PWM);
-   setup_ccp2(CCP_PWM);
-   
-   motorDirection('D',50,'D',50);
-   STBY = 1;
-   
-   delay_ms(2000);
-}
-
-void motorDirection(char M1, int pwm1, char M2, int pwm2)
-{
-   switch (M1)
+   if(OERR)
    {
-      case 'D': {BI1=1; BI2=0; break;}
-      case 'R': {BI1=0; BI2=1; break;}
-      case 'N': {BI1=0; BI2=0; break;}
+      getc();     //Clear buffer
+      getc();
+      CREN=0;      //Clear CREN bit
+      CREN=1;           
    }
-   
-   switch (M2)
-   {
-      case 'D': {AI1=1; AI2=0; break;}
-      case 'R': {AI1=0; AI2=1; break;}
-      case 'N': {AI1=0; AI2=0; break;}
-   }
-   
-   set_pwm1_duty(pwm1);
-   set_pwm2_duty(pwm2);
 }
---
+++ S_Sensor.c
@@ -0,0 +1,39 @@
+#include <18f4550.h>
+#fuses INTRC_IO, CPUDIV1, NOPROTECT, NOWDT, NOMCLR, NOLVP
+#use delay (clock=8M)
+
+#BYTE SSPBUF = 0xFC9
+
+/*
+// comandos
+#define ADC    1
+#define CONT   2
+#define PUSH   3
+*/
+int data;
+
+#int_ssp
+void spi_rcv()
+{  
+   data = SSPBUF;
+   
+   switch(data)
+   {
+      case 1:
+         output_toggle(PIN_D0);
+      break;
+   }  
+}
+
+void main()
+{
+   setup_spi(spi_slave | spi_L_to_H);
+   enable_interrupts(INT_SSP);
+   enable_interrupts(GLOBAL);
+   
+   while(true)
+   {
+   }
+   
+}
+
